ae4fdf25e2449e37b29bb7c10be1e2ec06d16d41
GhzGarage <gigahertzgarage@mail.com>
2021-10-12 21:32:22 -0500

    Initial Commit

---
+++ qbcore.sql
@@ -0,0 +1,147 @@
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `license` varchar(255) NOT NULL,
+  `permission` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `license` (`license`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `bans` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `license` varchar(50) DEFAULT NULL,
+  `discord` varchar(50) DEFAULT NULL,
+  `ip` varchar(50) DEFAULT NULL,
+  `reason` text DEFAULT NULL,
+  `expire` int(11) DEFAULT NULL,
+  `bannedby` varchar(255) NOT NULL DEFAULT 'Anticheat',
+  PRIMARY KEY (`id`),
+  KEY `license` (`license`),
+  KEY `discord` (`discord`),
+  KEY `ip` (`ip`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `bank_accounts` (
+  `record_id` bigint(255) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(250) DEFAULT NULL,
+  `buisness` varchar(50) DEFAULT NULL,
+  `buisnessid` int(11) DEFAULT NULL,
+  `gangid` varchar(50) DEFAULT NULL,
+  `amount` bigint(255) NOT NULL DEFAULT 0,
+  `account_type` enum('Current','Savings','Buisness','Gang') NOT NULL DEFAULT 'Current',
+  PRIMARY KEY (`record_id`),
+  UNIQUE KEY `citizenid` (`citizenid`),
+  KEY `buisness` (`buisness`),
+  KEY `buisnessid` (`buisnessid`),
+  KEY `gangid` (`gangid`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `bank_statements` (
+  `record_id` bigint(255) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `account` varchar(50) DEFAULT NULL,
+  `buisness` varchar(50) DEFAULT NULL,
+  `buisnessid` int(11) DEFAULT NULL,
+  `gangid` varchar(50) DEFAULT NULL,
+  `deposited` int(11) DEFAULT NULL,
+  `withdraw` int(11) DEFAULT NULL,
+  `balance` int(11) DEFAULT NULL,
+  `date` varchar(50) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`record_id`),
+  KEY `buisness` (`buisness`),
+  KEY `buisnessid` (`buisnessid`),
+  KEY `gangid` (`gangid`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(255) NOT NULL,
+  `cid` int(11) DEFAULT NULL,
+  `license` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `money` text NOT NULL,
+  `charinfo` text DEFAULT NULL,
+  `job` text NOT NULL,
+  `gang` text DEFAULT NULL,
+  `position` text NOT NULL,
+  `metadata` text NOT NULL,
+  `inventory` longtext DEFAULT NULL,
+  `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`citizenid`),
+  KEY `id` (`id`),
+  KEY `last_updated` (`last_updated`),
+  KEY `license` (`license`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `playerskins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(255) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `skin` text NOT NULL,
+  `clothes` text NOT NULL,
+  `active` tinyint(4) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`),
+  KEY `active` (`active`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `player_outfits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `outfitname` varchar(50) NOT NULL,
+  `model` varchar(50) DEFAULT NULL,
+  `skin` text DEFAULT NULL,
+  `outfitId` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`),
+  KEY `outfitId` (`outfitId`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `player_vehicles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `license` varchar(50) DEFAULT NULL,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `vehicle` varchar(50) DEFAULT NULL,
+  `hash` varchar(50) DEFAULT NULL,
+  `mods` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `plate` varchar(50) NOT NULL,
+  `fakeplate` varchar(50) DEFAULT NULL,
+  `garage` varchar(50) DEFAULT NULL,
+  `fuel` int(11) DEFAULT 100,
+  `engine` float DEFAULT 1000,
+  `body` float DEFAULT 1000,
+  `state` int(11) DEFAULT 1,
+  `depotprice` int(11) NOT NULL DEFAULT 0,
+  `drivingdistance` int(50) DEFAULT NULL,
+  `status` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `plate` (`plate`),
+  KEY `citizenid` (`citizenid`),
+  KEY `license` (`license`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `gloveboxitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(255) NOT NULL DEFAULT '[]',
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`plate`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `stashitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stash` varchar(255) NOT NULL DEFAULT '[]',
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`stash`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `trunkitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(255) NOT NULL DEFAULT '[]',
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`plate`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
