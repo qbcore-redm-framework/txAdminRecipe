a87a77ff18418f6154295c808b132b08a0c28965
zWyzzzzz <42010572+zWyzzzzz@users.noreply.github.com>
2022-03-16 08:19:34 +0100

    added horses.sql

--- qbrcore.sql
+++ qbrcore.sql
@@ -155,3 +155,13 @@ CREATE TABLE IF NOT EXISTS `management_menu` (
   UNIQUE KEY `job_name` (`job_name`),
   KEY `menu_type` (`menu_type`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `horses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cid` varchar(50) NOT NULL,
+  `selected` int(11) NOT NULL DEFAULT 0,
+  `model` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `components`  varchar(5000) NOT NULL DEFAULT '{}',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
